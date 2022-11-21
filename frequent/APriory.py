e0454c1256687799f93f852d0cf5a93e09226ad6
Sofia Krylova <sofia_krylova@epam.com>
2022-11-21 21:08:22 +0100

    HW2 done.

--- frequent/APriory.py
+++ frequent/APriory.py
@@ -1,10 +1,15 @@
+import itertools
+
+
 class APriory:
 
-    def __init__(self):
+    def __init__(self, support):
         self.transactions = None
+        self.single_hash_map = None
+        self.support = support
 
-    def read_file(self):
-
+    # Read a file and build L1 set
+    def process_file(self):
         transactions = []
         hash_map = {}
         with open('./data/T10I4D100K.dat') as file:
@@ -12,29 +17,65 @@ class APriory:
                 basket = line.split()
                 for item in basket:
                     if hash_map.get(item) is not None:
-                        counter = hash_map.get(item)
-                        hash_map[item] = counter + 1
+                        hash_map[item] += 1
                     else:
                         hash_map[item] = 1
                 transactions.append(basket)
+        transactions.sort()
         self.transactions = transactions
-        print("Successful read.")
-        return hash_map
+        self.single_hash_map = self.count_ln_support(hash_map, self.support)
+        print("Singleton number: " + str(len(self.single_hash_map)))
+        return self.single_hash_map
 
-    def clean_up_map(self, hash_map, threshold):
-        new_hash_map = {}
-        pair_map = {}
-        for key in hash_map.keys():
-            if hash_map.get(key) > threshold:
-                new_hash_map[key] = hash_map.get(key)
-        pairs = list(zip(*[iter(new_hash_map.keys())] * 2))
+    # Generate singletons
+    @staticmethod
+    def generate_l1(ln_list):
+        singletons = set()
+        for key in ln_list.keys():
+            if isinstance(key, tuple):
+                for item in key:
+                    singletons.add(item)
+            else:
+                singletons.add(key)
+        return singletons
 
-        for pair in pairs:
-            for transaction in self.transactions:
-                if set(pair).issubset(set(transaction)):
-                    if pair_map.get(pair) is not None:
-                        counter = pair_map.get(pair)
-                        pair_map[pair] = counter + 1
+    # Generate Ln set
+    def generate_ln_sets(self, ln_1, n):
+        ln = {}
+        # Generate singletons to check within each basket
+        l1 = self.generate_l1(ln_1)
+        for basket in self.transactions:
+            support_basket = sorted(l1.intersection(basket))
+            # Create a next candidates generation
+            candidates = sorted(itertools.combinations(support_basket, n))
+            for candidate in candidates:
+                # Check if a candidate is present in a previous Ln set
+                if self.check_smaller_candidates(candidate, ln_1, n):
+                    if candidate in ln:
+                        ln[candidate] += 1
                     else:
-                        pair_map[pair] = 1
-        return pair_map
+                        ln[candidate] = 1
+        # Count supported items
+        return self.count_ln_support(ln, self.support)
+
+    @staticmethod
+    def count_ln_support(l_map, threshold):
+        for key in list(l_map):
+            # if isinstance(key, tuple):
+            #     key = tuple(sorted(key))
+            if l_map.get(key) < threshold:
+                del l_map[key]
+        return l_map
+
+    @staticmethod
+    def check_smaller_candidates(item, ln_1, n):
+        combinations = sorted(itertools.combinations(list(item), n - 1))
+        for key in combinations:
+            # Process 1-tuple
+            if len(key) == 1:
+                if key[0] not in ln_1:
+                    return False
+            # Process n-tuple
+            elif key not in ln_1:
+                return False
+        return True
--- frequent/test.py
+++ frequent/test.py
@@ -1,6 +1,34 @@
+import time
+
 from frequent.APriory import APriory
+from frequent.Associations import Associations
 
 if __name__ == '__main__':
-    apriory = APriory()
-    hash_map = APriory.read_file(apriory)
-    APriory.clean_up_map(apriory, hash_map, 1000)
+    support = 200
+    confidence = 0.5
+    apriory = APriory(support)
+    frequent_items = []
+
+    start = time.time()
+    # Created transactions and supported singletons
+    ln = APriory.process_file(apriory)
+
+    # Run until no supported sets are left
+    n = 2
+    while len(ln) > 0:
+        k = 0
+        for item in ln:
+            k += 1
+            print("Items  " + str(k) + ": " + str(item) + " -> " + str(ln.get(item)))
+        frequent_items.append(ln)
+        ln = APriory.generate_ln_sets(apriory, ln, n)
+        n += 1
+
+    end = time.time() - start
+    print("Spent time " + str(round(end, 3)) + "s.")
+
+    rules = Associations.get_rules(Associations(), frequent_items, confidence)
+    k = 0
+    for rule in rules:
+        k += 1
+        print("Rule " + str(k) + ": " + str(rule) + " -> " + str(rules.get(rule)))
